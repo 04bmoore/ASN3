e4c4f5fbbbc5f8c04f765d5dc1f94380089f8f52
Brendan <bsmoore@loyola.edu>
2025-12-04 17:41:03 -0500

    Assignment 3

---
+++ src/main/java/edu/loyola/cs485/Main.java
@@ -0,0 +1,19 @@
+package edu.loyola.cs485;
+
+import java.sql.*;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static String ConUrl = "jdbc:mysql://localhost"; //protocol + url
+    static String Port = "3306"; //default MySQL port
+    static String Database = "soccer_group_asn"; // database/schema name
+    static String Username = "bsmoore"; //read this from a local file
+    static String Password = "1223711RMCF$"; //Also read this from a file
+
+    static String url = ConUrl+":"+Port+"/"+Database+ "?user="+Username+"&password="+Password;
+
+
+    static void main() {
+
+    }
+}
